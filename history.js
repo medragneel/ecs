29cfba1b11ab0d6d489df091af035d8dd2e89dec
mshadow <medmystgon@gmail.com>
2023-02-07 19:08:50 +0100

    v2.3

--- history.js
+++ history.js
@@ -1,7 +1,50 @@
+const tbs=document.querySelector('table');
+const btn= document.createElement('a');
+btn.setAttribute('id','crx-download-table-btn')
+btn.setAttribute('class','m-2 py-4')
+btn.innerHTML = 'download'
+const body= document.querySelector('body')
+body.appendChild(btn)
+
+
+
+
+function download_table_as_csv(table_id, separator = ',') {
+    // Select rows from table_id
+    var rows = document.querySelectorAll('table tr');
+    // Construct csv
+    var csv = [];
+    for (var i = 0; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll('td, th');
+        for (var j = 0; j < cols.length; j++) {
+            // Clean innertext to remove multiple spaces and jumpline (break csv)
+            var data = cols[j].innerText.replace(/(\r\n|\n|\r)/gm, '').replace(/(\s\s)/gm, ' ')
+            // Escape double-quote with double-double-quote (see https://stackoverflow.com/questions/17808511/properly-escape-a-double-quote-in-csv)
+            data = data.replace(/"/g, '""');
+            // Push escaped string
+            row.push('"' + data + '"');
+        }
+        csv.push(row.join(separator));
+    }
+    var csv_string = csv.join('\n');
+    // Download it
+    var filename = 'export_' + table_id + '_' + new Date().toLocaleDateString() + '.csv';
+    var link = document.createElement('a');
+    link.style.display = 'none';
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
+    link.setAttribute('download', filename);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+
 
 const history = JSON.parse(localStorage.getItem("info")) ?? {}
 
 const tab = document.createElement("table")
+tab.setAttribute("class","history")
 
 const tr1 = document.createElement("tr")
 const th1 = document.createElement("th")
@@ -28,6 +71,12 @@ for (const [key, value] of Object.entries(history)) {
 
 }
 
-document.body.appendChild(tab)
+body.appendChild(tab)
 
+
+document.querySelector('#crx-download-table-btn').addEventListener('click', e => {
+    e.preventDefault()
+    download_table_as_csv(".history")
+    
+})
 
